8eb48c750599e50fb360fea57ebaabd88f81675b
Lodrean <lodrean@yandex.ru>
2024-07-08 16:12:21 +0300

    Добавлена обработка кнопки "Назад" на тех экранах где она присутствует

--- app/src/main/java/ru/practicum/android/diploma/ui/vacancy/VacancyDetailsFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/vacancy/VacancyDetailsFragment.kt
@@ -5,6 +5,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import ru.practicum.android.diploma.databinding.FragmentVacancyDetailsBinding
 
 class VacancyDetailsFragment : Fragment() {
@@ -16,6 +17,12 @@ class VacancyDetailsFragment : Fragment() {
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding = FragmentVacancyDetailsBinding.inflate(inflater, container, false)
         return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        binding.topAppBar.setNavigationOnClickListener {
+            findNavController().popBackStack()
+        }
     }
 
     override fun onDestroyView() {
