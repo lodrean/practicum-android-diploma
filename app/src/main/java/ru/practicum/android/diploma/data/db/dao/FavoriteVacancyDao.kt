4b336849104afee2b6104fa9a2ad3b0dc4d9050f
bukholovds <bukholovds@gmail.com>
2024-07-08 10:40:13 +0300

    task-3: Создание базы данных. * Убрал Flow из Dao-интерфейсов;

--- app/src/main/java/ru/practicum/android/diploma/data/db/dao/FavoriteVacancyDao.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/db/dao/FavoriteVacancyDao.kt
@@ -5,7 +5,6 @@ import androidx.room.Delete
 import androidx.room.Insert
 import androidx.room.OnConflictStrategy
 import androidx.room.Query
-import kotlinx.coroutines.flow.Flow
 import ru.practicum.android.diploma.data.db.entity.FavoriteVacancyEntity
 
 @Dao
@@ -14,7 +13,7 @@ interface FavoriteVacancyDao {
     fun insertVacancy(vacancy: FavoriteVacancyEntity)
 
     @Query("select * from favorite_vacancies_table")
-    fun getFavoriteVacancies(): Flow<List<FavoriteVacancyEntity>>
+    fun getFavoriteVacancies(): List<FavoriteVacancyEntity>
 
     @Delete
     fun deleteFromFavorite(vacancy: FavoriteVacancyEntity)
--- app/src/main/java/ru/practicum/android/diploma/data/db/dao/FavoriteVacancyPhoneDao.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/db/dao/FavoriteVacancyPhoneDao.kt
@@ -3,13 +3,12 @@ package ru.practicum.android.diploma.data.db.dao
 import androidx.room.Dao
 import androidx.room.Insert
 import androidx.room.Query
-import kotlinx.coroutines.flow.Flow
 import ru.practicum.android.diploma.data.db.entity.FavoriteVacancyPhoneEntity
 
 @Dao
 interface FavoriteVacancyPhoneDao {
     @Query("select * from favorite_vacancy_phone_table where vacancy_id = :vacancyId")
-    fun getPhones(vacancyId: Int): Flow<List<FavoriteVacancyPhoneEntity>>
+    fun getPhones(vacancyId: Int): List<FavoriteVacancyPhoneEntity>
 
     @Insert
     fun insertPhone(favoriteVacancyPhoneEntity: FavoriteVacancyPhoneEntity)
