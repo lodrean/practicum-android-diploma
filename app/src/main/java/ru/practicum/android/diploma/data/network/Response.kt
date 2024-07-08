4e59334c4ffecd85d5fe3aef31784229a30ad110
herrfreeman <freeman@blackmesa.ru>
2024-07-08 13:01:43 +0300

    task-5: Fix default result code

--- app/src/main/java/ru/practicum/android/diploma/data/network/Response.kt
+++ app/src/main/java/ru/practicum/android/diploma/data/network/Response.kt
@@ -1,5 +1,9 @@
 package ru.practicum.android.diploma.data.network
 
 open class Response {
-    var resultCode = 0
+    var resultCode = DEFAULT_RESULT
+
+    companion object {
+        private const val DEFAULT_RESULT = 0
+    }
 }
