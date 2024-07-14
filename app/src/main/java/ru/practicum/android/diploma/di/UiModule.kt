4ee7a7238884e3c6643c7a53760766c68f46db42
IvanPodlesnykh <podlesnihwork@gmail.com>
2024-07-14 22:57:07 +0500

    Task-14 Экран Избранное 6. Сделал ViewModel для экрана избранных вакансий 7. Описал логику отображения информации на экране избранных вакансий

--- app/src/main/java/ru/practicum/android/diploma/di/UiModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/UiModule.kt
@@ -3,11 +3,16 @@ package ru.practicum.android.diploma.di
 import org.koin.android.ext.koin.androidApplication
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
+import ru.practicum.android.diploma.presentation.favorites.FavoritesViewModel
 import ru.practicum.android.diploma.ui.search.SearchViewModel
 
 val uiModule = module {
 
     viewModel<SearchViewModel> {
         SearchViewModel(get(), androidApplication())
+    }
+
+    viewModel<FavoritesViewModel> {
+        FavoritesViewModel(get())
     }
 }
--- app/src/main/java/ru/practicum/android/diploma/ui/favorites/FavoritesFragment.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/favorites/FavoritesFragment.kt
@@ -4,14 +4,23 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.view.isVisible
 import androidx.fragment.app.Fragment
+import org.koin.androidx.viewmodel.ext.android.viewModel
+import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.databinding.FragmentFavoritesBinding
+import ru.practicum.android.diploma.domain.models.Vacancy
+import ru.practicum.android.diploma.presentation.favorites.FavoritesState
+import ru.practicum.android.diploma.presentation.favorites.FavoritesViewModel
+import ru.practicum.android.diploma.ui.VacancyAdapter
 
 class FavoritesFragment : Fragment() {
 
     private var _binding: FragmentFavoritesBinding? = null
     private val binding
         get() = _binding!!
+
+    private val viewModel by viewModel<FavoritesViewModel>()
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
         _binding = FragmentFavoritesBinding.inflate(inflater, container, false)
@@ -22,4 +31,62 @@ class FavoritesFragment : Fragment() {
         super.onDestroyView()
         _binding = null
     }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        viewModel.getStateLiveData().observe(viewLifecycleOwner) {
+            when (it) {
+                is FavoritesState.Content -> showContent(it.vacanciesList)
+                is FavoritesState.Empty -> showEmptyMessage()
+                is FavoritesState.Error -> showError()
+                is FavoritesState.Loading -> showLoading()
+            }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        viewModel.updateFavoritesList()
+    }
+
+    private fun showContent(listOfVacancies: List<Vacancy>) {
+        val adapter = VacancyAdapter(listOfVacancies) {
+
+        }
+
+        binding.favoritesList.adapter = adapter
+
+        binding.errorImage.isVisible = false
+        binding.errorText.isVisible = false
+        binding.loading.isVisible = false
+        binding.favoritesList.isVisible = true
+    }
+
+    private fun showEmptyMessage() {
+        binding.errorImage.setImageResource(R.drawable.favorites_empty_list_image)
+        binding.errorText.setText(R.string.empty_favorites_list)
+
+        binding.errorImage.isVisible = true
+        binding.errorText.isVisible = true
+        binding.loading.isVisible = false
+        binding.favoritesList.isVisible = false
+    }
+
+    private fun showError() {
+        binding.errorImage.setImageResource(R.drawable.empty_list_image)
+        binding.errorText.setText(R.string.favorites_error)
+
+        binding.errorImage.isVisible = true
+        binding.errorText.isVisible = true
+        binding.loading.isVisible = false
+        binding.favoritesList.isVisible = false
+    }
+
+    private fun showLoading() {
+        binding.errorImage.isVisible = false
+        binding.errorText.isVisible = false
+        binding.loading.isVisible = true
+        binding.favoritesList.isVisible = false
+    }
+
 }
