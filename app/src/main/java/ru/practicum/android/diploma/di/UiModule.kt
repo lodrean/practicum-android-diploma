73b8b9a3f7a418b1cb54b4ba889c06f57b003ebd
bukholovds <bukholovds@gmail.com>
2024-07-14 18:55:32 +0300

    task-15: Экран детали вакансии * Замечания reviewdog

--- app/src/main/java/ru/practicum/android/diploma/di/UiModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/UiModule.kt
@@ -3,9 +3,9 @@ package ru.practicum.android.diploma.di
 import org.koin.android.ext.koin.androidApplication
 import org.koin.androidx.viewmodel.dsl.viewModel
 import org.koin.dsl.module
-import ru.practicum.android.diploma.ui.search.SearchViewModel
 import ru.practicum.android.diploma.domain.models.Vacancy
 import ru.practicum.android.diploma.presentation.VacancyDetailsViewModel
+import ru.practicum.android.diploma.ui.search.SearchViewModel
 
 val uiModule = module {
 
