19b174a0683981f6c2d6a3d63226e0c0b1b47059
bukholovds <bukholovds@gmail.com>
2024-07-15 17:57:32 +0300

    task-21: Взаимодействие на экране детали вакансии. * Замечания reviewdog

--- app/src/main/java/ru/practicum/android/diploma/di/RepositoryModule.kt
+++ app/src/main/java/ru/practicum/android/diploma/di/RepositoryModule.kt
@@ -1,6 +1,7 @@
 package ru.practicum.android.diploma.di
 
 import org.koin.dsl.module
+import ru.practicum.android.diploma.data.FavoritesRepositoryImpl
 import ru.practicum.android.diploma.data.VacanciesRepositoryImpl
 import ru.practicum.android.diploma.data.sharing.SharingRepository
 import ru.practicum.android.diploma.data.sharing.SharingRepositoryImpl
