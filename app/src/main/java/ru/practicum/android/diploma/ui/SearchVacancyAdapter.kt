5bdf1338e07a2492fa4f22494a531a0aa27946aa
Lodrean <lodrean@yandex.ru>
2024-07-16 21:55:06 +0300

    Task-18 прокрутка

--- app/src/main/java/ru/practicum/android/diploma/ui/SearchVacancyAdapter.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/SearchVacancyAdapter.kt
@@ -23,7 +23,7 @@ class SearchVacancyAdapter(private val onItemClickListener: OnItemClickListener)
 
     fun showLoading(isShowLoading: Boolean) {
         showLoading = isShowLoading
-        notifyItemChanged(listData.size + 2)
+        notifyItemChanged(listData.size + 1)
     }
 
     override fun getItemViewType(position: Int) =
