b657fe2461915adef5e681b8b1095a50ecab16de
Lodrean <lodrean@yandex.ru>
2024-07-20 16:14:19 +0300

    Task 35: добавлена настройка фокуса edittext

--- app/src/main/java/ru/practicum/android/diploma/ui/root/RootActivity.kt
+++ app/src/main/java/ru/practicum/android/diploma/ui/root/RootActivity.kt
@@ -27,26 +27,6 @@ class RootActivity : AppCompatActivity() {
     private val binding
         get() = _binding!!
 
-    // функция проверяет, находится ли точка касания внутри области видимости текстового поля.
-    // Если точка касания находится вне области видимости текстового поля,
-    // то функция снимает фокус с текстового поля и скрывает клавиатуру.
-
-    override fun dispatchTouchEvent(event: MotionEvent): Boolean {
-        if (event.action == MotionEvent.ACTION_DOWN) {
-            val v = currentFocus
-            if (v is TextInputEditText) {
-                val outRect = Rect()
-                v.getGlobalVisibleRect(outRect)
-                if (!outRect.contains(event.rawX.toInt(), event.rawY.toInt())) {
-                    v.clearFocus()
-                    val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
-                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0)
-                }
-            }
-        }
-        return super.dispatchTouchEvent(event)
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         _binding = ActivityRootBinding.inflate(layoutInflater)
@@ -166,5 +146,23 @@ class RootActivity : AppCompatActivity() {
 //            }
         }
     }
+// функция проверяет, находится ли точка касания внутри области видимости текстового поля.
+    // Если точка касания находится вне области видимости текстового поля,
+    // то функция снимает фокус с текстового поля и скрывает клавиатуру.
 
+    override fun dispatchTouchEvent(event: MotionEvent): Boolean {
+        if (event.action == MotionEvent.ACTION_DOWN) {
+            val v = currentFocus
+            if (v is TextInputEditText) {
+                val outRect = Rect()
+                v.getGlobalVisibleRect(outRect)
+                if (!outRect.contains(event.rawX.toInt(), event.rawY.toInt())) {
+                    v.clearFocus()
+                    val imm = getSystemService(INPUT_METHOD_SERVICE) as InputMethodManager
+                    imm.hideSoftInputFromWindow(v.getWindowToken(), 0)
+                }
+            }
+        }
+        return super.dispatchTouchEvent(event)
+    }
 }
