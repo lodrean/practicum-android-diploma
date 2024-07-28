7dd4fdfba5351d3e2ce89a81579434380283ad39
Lodrean <lodrean@yandex.ru>
2024-07-28 11:34:24 +0300

    task-53 проставлены знаки валюты

--- app/src/main/java/ru/practicum/android/diploma/util/UtilityFunctions.kt
+++ app/src/main/java/ru/practicum/android/diploma/util/UtilityFunctions.kt
@@ -1,19 +1,32 @@
 package ru.practicum.android.diploma.util
 
 import android.content.Context
+import androidx.core.content.ContextCompat.getString
 import ru.practicum.android.diploma.R
 import ru.practicum.android.diploma.domain.models.Vacancy
 import java.text.DecimalFormat
 import java.util.Currency
-import java.util.Locale
 
 object UtilityFunctions {
 
     fun formatSalary(vacancy: Vacancy, context: Context): String {
+        val currencySymbol: MutableMap<String, String> = hashMapOf(
+            context.getString(R.string.eur) to context.getString(R.string.eur_symbol),
+            context.getString(R.string.usd) to context.getString(R.string.usd_symbol),
+            context.getString(R.string.rur) to context.getString(R.string.rur_symbol),
+            context.getString(R.string.rub) to context.getString(R.string.rub_symbol),
+            context.getString(R.string.byr) to context.getString(R.string.byr_symbol),
+            context.getString(R.string.kzt) to context.getString(R.string.kzt_symbol),
+            context.getString(R.string.uah) to context.getString(R.string.uah_symbol),
+            context.getString(R.string.azn) to context.getString(R.string.azn_symbol),
+            context.getString(R.string.uzs) to context.getString(R.string.uzs_symbol),
+            context.getString(R.string.gel) to context.getString(R.string.gel_symbol),
+            context.getString(R.string.kgt) to context.getString(R.string.kgt_symbol),
+        )
         var symbol = ""
         if (vacancy.salaryCurrencyName != null) {
             val currency = Currency.getInstance(vacancy.salaryCurrencyName)
-            symbol = currency.getSymbol(Locale.getDefault(Locale.Category.DISPLAY))
+            symbol = currencySymbol[currency.currencyCode] ?: getString(context, R.string.empty_string)
         }
 
         val decimalFormat = DecimalFormat("#,###.##")
